cc8a9ac948dbb8c28fcd05c32a5df2a8601fc8c4
宮澤昭海 <>
2023-01-20 07:19:29 +0900

    記事一覧を表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create({title: '新しい記事', content: 'いい記事'})
+Article.create({title: '新しい記事2', content: 'いい記事2'})
