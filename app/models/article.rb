2c2a77fe89b889d105a4ade5966ca8e985bac54a
宮澤昭海 <>
2023-04-29 21:53:47 +0900

    contentのValidatesの削除とscssの変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,10 +21,7 @@ class Article < ApplicationRecord
   validates :title, format: { with: /\A(?!@)/ }
 
   validates :content, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :content, uniqueness: true
 
-  validate :validate_title_and_content_length
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -40,12 +37,5 @@ class Article < ApplicationRecord
 
   def like_count
     likes.count
-  end
-
-  private
-
-  def validate_title_and_content_length
-    char_count = title.length + content.length
-    errors.add(:content, '30文字以上で!') unless char_count > 30
   end
 end
