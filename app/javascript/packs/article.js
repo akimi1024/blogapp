a86f8569ab3a752316443cf992fda2724f29c4a5
宮澤昭海 <>
2023-06-02 14:07:00 +0900

    jsModulesを追加

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,8 +1,9 @@
 import $ from 'jquery'
-import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
-
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+import axios from 'modules/axios'
+import {
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent
+  } from 'modules/handle_heart.js'
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
@@ -62,31 +63,6 @@ document.addEventListener('DOMContentLoaded', () => {
       handleHeartDisplay(hasLiked)
     })
 
-  $('.inactive-heart').on('click', () => {
-    axios.post(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status == 'ok'){
-          $('.active-heart').removeClass('hidden')
-          $('.inactive-heart').addClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
-
-  $('.active-heart').on('click', () => {
-    axios.delete(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status == 'ok'){
-          $('.inactive-heart').removeClass('hidden')
-          $('.active-heart').addClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
+    listenInactiveHeartEvent(articleId)
+    listenActiveHeartEvent(articleId)
 })
