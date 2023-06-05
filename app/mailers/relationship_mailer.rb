07adf2679420c0fd96e498589904dbcb0252b4f1
宮澤昭海 <>
2023-06-05 23:23:57 +0900

    relationship_mailer.rbの修正

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -1,7 +1,7 @@
-class RelationshpMailer < ApplicationMailer
+class RelationshipMailer < ApplicationMailer
   def new_follower(user, follower)
     @user = user
     @follower = follower
-    mail to: user.email, subject: '[お知らせ]フォローされました'
+    mail to: user.email, subject: '【お知らせ】フォローされました'
   end
 end
